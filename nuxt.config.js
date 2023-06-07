560e6b2c1262dabd2a8f0366e072d7ad80a32c39
nasriandirian <riannasriandi@gmail.com>
2023-06-07 13:21:48 +0700

    new file:   assets/css/main.css 	deleted:    components/NuxtLogo.vue 	new file:   components/Sidebar.vue 	deleted:    components/Tutorial.vue 	new file:   layouts/dashboard.vue 	new file:   layouts/default.vue 	new file:   layouts/form.vue 	modified:   nuxt.config.js 	modified:   package-lock.json 	modified:   package.json 	new file:   pages/companies/_id/employees/create.vue 	new file:   pages/companies/_id/employees/finish.vue 	new file:   pages/companies/_id/employees/index.vue 	new file:   pages/companies/_id/employees/role.vue 	new file:   pages/companies/_id/employees/team.vue 	new file:   pages/companies/_id/index.vue 	new file:   pages/companies/_id/roles/create.vue 	new file:   pages/companies/_id/roles/index.vue 	new file:   pages/companies/_id/teams/create.vue 	new file:   pages/companies/_id/teams/index.vue 	new file:   pages/companies/create.vue 	new file:   pages/companies/index.vue 	modified:   pages/index.vue 	new file:   pages/login.vue 	new file:   pages/register.vue 	new file:   static/assets/images/user-f-1.png 	new file:   static/assets/images/user-f-2.png 	new file:   static/assets/images/user-f-3.png 	new file:   static/assets/images/user-f-4.png 	new file:   static/assets/images/user-m-1.png 	new file:   static/assets/images/user-m-2.png 	new file:   static/assets/images/user-m-3.png 	new file:   static/assets/images/user-m-4.png 	new file:   static/assets/svgs/ic-bell.svg 	new file:   static/assets/svgs/ic-box.svg 	new file:   static/assets/svgs/ic-briefcase.svg 	new file:   static/assets/svgs/ic-check-circle.svg 	new file:   static/assets/svgs/ic-chevron-down.svg 	new file:   static/assets/svgs/ic-dollar.svg 	new file:   static/assets/svgs/ic-flag.svg 	new file:   static/assets/svgs/ic-gift.svg 	new file:   static/assets/svgs/ic-grid.svg 	new file:   static/assets/svgs/ic-home.svg 	new file:   static/assets/svgs/ic-search.svg 	new file:   static/assets/svgs/ic-settings.svg 	new file:   static/assets/svgs/ic-signout.svg 	new file:   static/assets/svgs/ic-users.svg 	new file:   static/assets/svgs/logo-mark.svg 	new file:   static/assets/svgs/logo-type.svg 	new file:   static/assets/svgs/ric-archive.svg 	new file:   static/assets/svgs/ric-award.svg 	new file:   static/assets/svgs/ric-book.svg 	new file:   static/assets/svgs/ric-box.svg 	new file:   static/assets/svgs/ric-close-red.svg 	new file:   static/assets/svgs/ric-close-white.svg 	new file:   static/assets/svgs/ric-database.svg 	new file:   static/assets/svgs/ric-download.svg 	new file:   static/assets/svgs/ric-flag.svg 	new file:   static/assets/svgs/ric-globe.svg 	new file:   static/assets/svgs/ric-plus.svg 	new file:   static/assets/svgs/ric-target.svg 	new file:   static/assets/svgs/ric-twitch.svg 	new file:   tailwind.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@/assets/css/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -32,7 +32,8 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    // '@nuxtjs/tailwindcss',
+    '@nuxt/postcss8',
     '@nuxtjs/dotenv',
   ],
 
@@ -46,9 +47,41 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'http://powerhuman-backend.test/api/',
+  },
+
+  //login
+  auth: {
+    strategies: {
+      local: {
+        token: {
+          property: 'result.access_token',
+          global: true,
+          required: true,
+          type: 'Bearer',
+        },
+        user: {
+          property: 'result',
+          autoFetch: true,
+        },
+        endpoints: {
+          login: { url: '/login', method: 'post' },
+          logout: { url: '/logout', method: 'post' },
+          user: { url: '/fetch', method: 'get' },
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
 }
